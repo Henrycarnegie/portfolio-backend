95cc587b41616220774aac01a520410e7e329654
Henry Carnegie <henrycarnegie23@gmail.com>
2025-08-24 13:24:06 +0700

    change: delete index.js and using admin.js on folder api

--- api/admin.js
+++ api/admin.js
@@ -1,15 +1,14 @@
 import express from "express";
 import cors from "cors";
-import adminRoute from "./routes/adminRoute.js";
-import dotenv from "dotenv";
-
-dotenv.config();
+import adminRoute from "../routes/adminRoute.js";
 
 const app = express();
 
+// middleware
 app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000" }));
 app.use(express.json());
 
-app.use(adminRoute);
+// routes
+app.use("/api", adminRoute);
 
 export default app;
