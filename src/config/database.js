dd060a826628f969dbcdba7054340705fdd23019
Henry Carnegie <henrycarnegie23@gmail.com>
2025-08-24 16:15:42 +0700

    change: change database config to env direct

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,13 @@
 import dotenv from "dotenv";
 import mysql from "mysql2";
-import fs from "fs";
-import path from "path"; 
 
 dotenv.config();
 
 let sequelize;
-console.log("=== DEBUG DB_SSL_CA ===");
-console.log("Raw env:", process.env.DB_SSL_CA);
 
 export async function getSequelize() {
    if (!sequelize) {
       const { Sequelize } = await import("sequelize");
-
-      // Resolve relative path ke absolute path
-      const caPath = path.resolve(process.env.DB_SSL_CA);
-      console.log("Resolved CA path:", caPath);
-
       sequelize = new Sequelize(
          process.env.DB_NAME,
          process.env.DB_USER,
@@ -29,7 +20,7 @@ export async function getSequelize() {
             logging: false,
             dialectOptions: {
                ssl: {
-                 ca: fs.readFileSync(caPath, "utf8")
+                  ca: process.env.DB_SSL_CA,
                },
             },
          }
