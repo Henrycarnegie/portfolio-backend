e825781fa9cd6ca09bf607a9db380866eb87eb73
Henry Carnegie <henrycarnegie23@gmail.com>
2025-08-24 15:00:22 +0700

    change

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,30 @@
 import dotenv from "dotenv";
-import mysql from "mysql2"; 
+import mysql from "mysql2";
+
 dotenv.config();
 
 let sequelize;
 
 export async function getSequelize() {
-  if (!sequelize) {
-    const { Sequelize } = await import("sequelize");
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: "mysql",
-        dialectModule: mysql, 
-        logging: false, 
-      }
-    );
-  }
-  return sequelize;
+   if (!sequelize) {
+      const { Sequelize } = await import("sequelize");
+      sequelize = new Sequelize(
+         process.env.DB_NAME,
+         process.env.DB_USER,
+         process.env.DB_PASSWORD,
+         {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            dialect: "mysql",
+            dialectModule: mysql,
+            logging: false,
+            dialectOptions: {
+               ssl: {
+                  ca: process.env.DB_SSL_CA,
+               },
+            },
+         }
+      );
+   }
+   return sequelize;
 }
-
--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import adminRoute from "./routes/adminRoute.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3001/*" }));
 app.use(express.json());
 
-// route utama
 app.use("/admin", adminRoute);
 
-// export app (bukan listen)
 export default app;
