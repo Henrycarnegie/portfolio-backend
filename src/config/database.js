ca0c364502491c478ce4ab1378caf26524ace42b
Henry Carnegie <henrycarnegie23@gmail.com>
2025-08-24 16:43:57 +0700

    change: change database config to env direct

--- src/config/database.js
+++ src/config/database.js
@@ -1,39 +1,30 @@
-import dotenv from "dotenv";
-import mysql from "mysql2";
-import fs from "fs";
-import path from "path"; 
+import dotenv from 'dotenv';
+import mysql from 'mysql2';
+import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
 let sequelize;
-console.log("=== DEBUG DB_SSL_CA ===");
-console.log("Raw env:", process.env.DB_SSL_CA);
 
 export async function getSequelize() {
-   if (!sequelize) {
-      const { Sequelize } = await import("sequelize");
-
-      // Resolve relative path ke absolute path
-      const caPath = path.resolve(process.env.DB_SSL_CA);
-      console.log("Resolved CA path:", caPath);
-
-      sequelize = new Sequelize(
-         process.env.DB_NAME,
-         process.env.DB_USER,
-         process.env.DB_PASSWORD,
-         {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            dialect: "mysql",
-            dialectModule: mysql,
-            logging: false,
-            dialectOptions: {
-               ssl: {
-                 ca: fs.readFileSync(caPath, "utf8")
-               },
-            },
-         }
-      );
-   }
-   return sequelize;
+  if (!sequelize) {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        dialectModule: mysql,
+        logging: false,
+        dialectOptions: {
+          ssl: {
+            ca: process.env.DB_SSL_CA
+          }
+        }
+      }
+    );
+  }
+  return sequelize;
 }
